4b85698804d6cac5fa1088e51248e836abea214f
Kaloyan <simeonov.kaloyan.pr@gmail.com>
2023-09-23 18:11:48 +0300

    Add start project

---
+++ RVFinder/RVFinder/RVFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RVFinderApp.swift
+//  RVFinder
+//
+//  Created by Evelina Zaneva on 23.09.23.
+//
+
+import SwiftUI
+
+@main
+struct RVFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RVSearchScreen()
+        }
+    }
+}
